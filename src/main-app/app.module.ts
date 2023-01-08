aa5045060e625041014b2c963f738c40f3cb6351
VladislavShults <shvs93@mail.ru>
2023-01-08 10:23:00 +0500

    first commit

--- src/main-app/app.module.ts
+++ src/main-app/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsPlatformModule } from '../infrastructure/modules/blogsPlatformModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BlogsPlatformModule],
+  imports: [
+    BlogsPlatformModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'sa',
+      database: 'BloggersPlatform',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-//TODO make it better
